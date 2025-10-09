429d453b7ab8f224b7d1d42cc68f6d18cf22592c
saam naeini <alpasaam422@gmail.com>
2025-10-09 08:57:44 -0600

    1110 finsihed

--- 1110.delete-nodes-and-return-forest.py
+++ 1110.delete-nodes-and-return-forest.py
@@ -3,6 +3,50 @@
 #
 # [1110] Delete Nodes And Return Forest
 #
+# https://leetcode.com/problems/delete-nodes-and-return-forest/description/
+#
+# algorithms
+# Medium (72.44%)
+# Likes:    4746
+# Dislikes: 143
+# Total Accepted:    380.8K
+# Total Submissions: 525.7K
+# Testcase Example:  '[1,2,3,4,5,6,7]\n[3,5]'
+#
+# Given the root of a binary tree, each node in the tree has a distinct value.
+# 
+# After deleting all nodes with a value in to_delete, we are left with a forest
+# (a disjoint union of trees).
+# 
+# Return the roots of the trees in the remaining forest. You may return the
+# result in any order.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: root = [1,2,3,4,5,6,7], to_delete = [3,5]
+# Output: [[1,2,null,4],[6],[7]]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: root = [1,2,4,null,3], to_delete = [3]
+# Output: [[1,2,4]]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of nodes in the given tree is at most 1000.
+# Each node has a distinct value between 1 and 1000.
+# to_delete.length <= 1000
+# to_delete contains distinct values between 1 and 1000.
+# 
+# 
+#
 
 # @lc code=start
 # Definition for a binary tree node.
@@ -13,21 +57,23 @@
 #         self.right = right
 class Solution:
     def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
-
         to_delete_set = set(to_delete)
         res = []
 
         def dfs(node, is_root):
             if not node:
                 return None
+            
             deleted = node.val in to_delete_set
             if is_root and not deleted:
                 res.append(node)
+            
             node.left = dfs(node.left, deleted)
             node.right = dfs(node.right, deleted)
             return None if deleted else node
-
+        
         dfs(root, True)
         return res
         
 # @lc code=end
+
